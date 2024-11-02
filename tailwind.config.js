d20ef53d089898b6bdde38a3a8a05ce03e0c7814
HarleyMartins <harleyfla05@gmail.com>
2024-11-02 16:31:39 -0300

    chore: cores personalizadas da Assesi adicionadas

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        headerBackground: "#212529",
+        navButton: "#2082f4",
+        navNotification: "#fab100",
+        aAmbientalBtn: "#005b41",
+        aCidadaoBtn: "#3846b1",
+        aContratacaoBtn: "#5a3a86",
+        aGedGovBtn: "#316191",
+        aLegislativoBtn: "#009d4d",
+        aSiteGovBtn: "#01698b",
+        InstaladorBg: "#f84233",
+        footerBg: "#1d1d1b",
+      },
+
+      fontFamily: {},
+    },
   },
   plugins: [],
 }
+
+// --font-quick: Quicksand, sans-serif;
+// --font-popp: Poppins, sans-serif;
+// --font-heeb: Heebo, sans-serif;
